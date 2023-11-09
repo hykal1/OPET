c8832f65e2b53edfd7b14244a5fa0509d361ee15
hykal1 <hykalhakim312@gmail.com>
2023-11-09 20:35:41 +0700

    sign in

---
+++ app/src/main/java/com/example/projectuas/login/SignIn.java
@@ -0,0 +1,15 @@
+package com.example.projectuas.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projectuas.R;
+
+public class SignIn extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
