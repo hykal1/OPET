a3e0f8c910194dbe59f76427e6d1041430d677bd
teguhrzo <pranantoteguh04@gmail.com>
2023-11-09 15:46:08 +0700

    Splash Screen

---
+++ app/src/main/java/com/example/projectuas/splash/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.projectuas.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.projectuas.MainActivity;
+import com.example.projectuas.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Intent untuk pindah ke activity selanjutnya
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                // Tutup activity splash screen
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
