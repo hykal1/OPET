901007d1576d8965645413ca0ff8d583ec17b552
teguhrzo <pranantoteguh04@gmail.com>
2023-11-10 23:22:16 +0700

    Fix and Revamp Login and Sign up

--- app/src/main/java/com/example/projectuas/login/SignIn.java
+++ app/src/main/java/com/example/projectuas/login/SignIn.java
@@ -1,6 +1,8 @@
 package com.example.projectuas.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -11,5 +13,12 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        fragment_login login = new fragment_login();
+        mFragmentTransaction.add(R.id.frame_login_signup, login, fragment_login.class.getSimpleName());
+
+        mFragmentTransaction.commit();
     }
 }
