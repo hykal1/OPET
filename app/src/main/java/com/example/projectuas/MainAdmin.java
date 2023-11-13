881d99b6782e0d37a6c5d912287e6cba2eb7c131
teguhrzo <pranantoteguh04@gmail.com>
2023-11-13 20:33:52 +0700

    Fix some error and Add Sales Report

--- app/src/main/java/com/example/projectuas/MainAdmin.java
+++ app/src/main/java/com/example/projectuas/MainAdmin.java
@@ -2,13 +2,17 @@ package com.example.projectuas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainAdmin extends AppCompatActivity {
+public class MainAdmin extends AppCompatActivity implements View.OnClickListener{
 
     TextView username;
-    public static String EXTRA_USERNAME = "USERNAM";
+    ImageButton report, order;
+    public static String EXTRA_USERNAME = "USERNAME";
 
 
     @Override
@@ -18,5 +22,17 @@ public class MainAdmin extends AppCompatActivity {
         String name = getIntent().getStringExtra(EXTRA_USERNAME);
         username = findViewById(R.id.username);
         username.setText("Hello, "+name);
+        report = findViewById(R.id.sales_report_menu);
+        order = findViewById(R.id.incoming_order_menu);
+        report.setOnClickListener(this);
+        order.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.sales_report_menu){
+            Intent report = new Intent(MainAdmin.this, sales_report.class);
+            startActivity(report);
+        }
     }
 }
